70f1c31485ae2eeb01a12dd86ab3825d4459688a
Aidy <aidyszh@gmail.com>
2019-04-17 11:35:53 +0800

    initial commit

---
+++ MyPluginStartUp.js
@@ -0,0 +1,15 @@
+function addMenu() {                 
+    var menuStr = '\n<menu id="MyPlugin" name="MyPlugin">'+
+    '\n<menuitem id="Test" name="Test" file="MyPlugin_TestMenu.htm"/>'+
+    '\n</menu>';
+
+    var menuFile = dreamweaver.getConfigurationPath() + "/Menus/Menus.xml";
+    var str = DWfile.read(menuFile);
+    if(str.indexOf('id="MyPlugin"') == -1){
+        if(str.indexOf('id="DWMenu_Help"') > -1){
+            var index = str.lastIndexOf('<menu ', str.indexOf('id="DWMenu_Help"'));
+            str = str.substring(0, index) + menuStr + str.substring(index);
+        }
+        DWfile.write(menuFile, str);
+    }   
+}
