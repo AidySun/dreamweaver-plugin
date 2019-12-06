2809391ab7535a79e39467ba8f1d7dd099e26ec5
Aidy <aidyszh@gmail.com>
2019-12-06 17:06:39 +0800

    bug

--- ucf-dreamweaver-plugin.sh
+++
@@ -1,2 +0,0 @@
-java -jar ./ucf-signature/signingtoolkit/ucf.jar -package -storetype PKCS12 -keystore ./ucf-signature/signature/keystore.pkcs12  -storepass iceprince  ./dreamweaver-plugin-sample.zxp -C ./dreamweaver-plugin/ .
-pause
