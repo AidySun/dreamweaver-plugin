014fd6b1ec48760686023c13bf90c84e3b525c45
Aidy <aidyszh@gmail.com>
2019-12-06 15:41:26 +0800

    refactor and add popup menu test

---
+++ ucf-dreamweaver-plugin.sh
@@ -0,0 +1,2 @@
+java -jar ./ucf-signature/signingtoolkit/ucf.jar -package -storetype PKCS12 -keystore ./ucf-signature/signature/keystore.pkcs12  -storepass iceprince  ./dreamweaver-plugin-sample.zxp -C ./dreamweaver-plugin/ .
+pause
