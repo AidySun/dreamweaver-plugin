39209e93d76b24307c442696f9ca2b2eb3b24c48
Aidy <aidyszh@gmail.com>
2019-12-06 17:40:19 +0800

    update

--- dreamweaver-plugin/cpfiles.sh
+++ dreamweaver-plugin/cpfiles.sh
@@ -4,9 +4,8 @@ installfolder="/Applications/Adobe Dreamweaver 2020"
 
 rm "$installfolder/configuration/Startup/MyPluginStartUp.js" "$installfolder/configuration/Startup/MyPluginStartup.htm"  "$installfolder/configuration/Commands/MyPluginCommand.htm" "$installfolder/configuration/Floaters/MyPluginFloater.htm" ~/Library/Application\ Support/Adobe/Dreamweaver\ 2020/en_US/Configuration/Commands/MyPluginCommand.htm ~/Library/Application\ Support/Adobe/Dreamweaver\ 2020/en_US/Configuration/Floaters/MyPluginFloater.htm
 
-
 cp ./MyPluginStartUp.js "$installfolder/configuration/Startup/"
 cp ./MyPluginStartup.htm "$installfolder/configuration/Startup/"
-cp ./MyPluginCommand.htm "$userinstallfolder/configuration/Commands/"
-cp ./MyPluginFloater.htm "$userinstallfolder/configuration/Floaters/"
+cp ./MyPluginCommand.htm "$installfolder/configuration/Commands/"
+cp ./MyPluginFloater.htm "$installfolder/configuration/Floaters/"
 		
